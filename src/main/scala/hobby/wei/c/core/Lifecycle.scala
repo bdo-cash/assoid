4d2a167aa7f6d86efe99571aaf8560f1f5c96803
Chenakam <chenai.nakam@gmail.com>
2017-12-25 22:38:05 +0800

    add |> Lifecycle 增加 Frgmt 和 Dialog 打印。

--- src/main/scala/hobby/wei/c/core/Lifecycle.scala
+++ src/main/scala/hobby/wei/c/core/Lifecycle.scala
@@ -16,10 +16,11 @@
 
 package hobby.wei.c.core
 
-import android.content.Context
+import android.app.{Activity, Fragment}
+import android.content.{Context, Intent}
 import android.os.{Bundle, PersistableBundle}
 import android.util.AttributeSet
-import android.view.View
+import android.view.{LayoutInflater, View, ViewGroup}
 import hobby.chenai.nakam.basis.TAG
 import hobby.wei.c.LOG._
 
@@ -30,18 +31,18 @@ import hobby.wei.c.LOG._
 object Lifecycle {
   trait Acty extends AbsActy with TAG.ClassName {
     override protected def onCreate(savedInstanceState: Bundle): Unit = {
-      super.onCreate(savedInstanceState)
       i("onCreate")
+      super.onCreate(savedInstanceState)
     }
 
     override protected def onCreate(savedInstanceState: Bundle, persistentState: PersistableBundle): Unit = {
-      super.onCreate(savedInstanceState, persistentState)
       i("onCreate2")
+      super.onCreate(savedInstanceState, persistentState)
     }
 
     override protected def onPostCreate(savedInstanceState: Bundle): Unit = {
-      super.onPostCreate(savedInstanceState)
       i("onPostCreate")
+      super.onPostCreate(savedInstanceState)
     }
 
     override protected def onCreateView(name: String, context: Context, attrs: AttributeSet) = {
@@ -54,24 +55,64 @@ object Lifecycle {
       super.onCreateView(parent, name, context, attrs)
     }
 
+    override protected def onActivityResult(requestCode: Int, resultCode: Int, data: Intent): Unit = {
+      i("onActivityResult")
+      super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override def onRequestPermissionsResult(requestCode: Int, permissions: Array[String], grantResults: Array[Int]): Unit = {
+      i("onRequestPermissionsResult")
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override protected def onSaveInstanceState(outState: Bundle): Unit = {
+      i("onSaveInstanceState")
+      super.onSaveInstanceState(outState)
+    }
+
+    override protected def onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle): Unit = {
+      i("onSaveInstanceState2")
+      super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override def onRestoreInstanceState(savedInstanceState: Bundle): Unit = {
+      i("onRestoreInstanceState")
+      super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override def onRestoreInstanceState(savedInstanceState: Bundle, persistentState: PersistableBundle): Unit = {
+      i("onRestoreInstanceState2")
+      super.onRestoreInstanceState(savedInstanceState, persistentState)
+    }
+
+    override protected def onUserInteraction(): Unit = {
+      i("onUserInteraction")
+      super.onUserInteraction()
+    }
+
+    override protected def onUserLeaveHint(): Unit = {
+      i("onUserLeaveHint")
+      super.onUserLeaveHint()
+    }
+
     override protected def onRestart(): Unit = {
-      super.onRestart()
       i("onRestart")
+      super.onRestart()
     }
 
     override protected def onStart(): Unit = {
-      super.onStart()
       i("onStart")
+      super.onStart()
     }
 
     override protected def onResume(): Unit = {
-      super.onResume()
       i("onResume")
+      super.onResume()
     }
 
     override protected def onPostResume(): Unit = {
-      super.onPostResume()
       i("onPostResume")
+      super.onPostResume()
     }
 
     override protected def onPause(): Unit = {
@@ -94,4 +135,98 @@ object Lifecycle {
       super.onDestroyToExit()
     }
   }
+
+  trait Frmgt extends Fragment with TAG.ClassName {
+    override def onAttach(activity: Activity): Unit = {
+      i("onAttach")
+      super.onAttach(activity)
+    }
+
+    override def onAttach(context: Context): Unit = {
+      i("onAttach2")
+      super.onAttach(context)
+    }
+
+    override def onAttachFragment(childFragment: Fragment): Unit = {
+      i("onAttachFragment")
+      super.onAttachFragment(childFragment)
+    }
+
+    override def onActivityCreated(savedInstanceState: Bundle): Unit = {
+      i("onActivityCreated")
+      super.onActivityCreated(savedInstanceState)
+    }
+
+    override def onActivityResult(requestCode: Int, resultCode: Int, data: Intent): Unit = {
+      i("onActivityResult")
+      super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override def onRequestPermissionsResult(requestCode: Int, permissions: Array[String], grantResults: Array[Int]): Unit = {
+      i("onRequestPermissionsResult")
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override def onSaveInstanceState(outState: Bundle): Unit = {
+      i("onSaveInstanceState")
+      super.onSaveInstanceState(outState)
+    }
+
+    override def onViewStateRestored(savedInstanceState: Bundle): Unit = {
+      i("onViewStateRestored")
+      super.onViewStateRestored(savedInstanceState)
+    }
+
+    override def onCreate(savedInstanceState: Bundle): Unit = {
+      i("onCreate")
+      super.onCreate(savedInstanceState)
+    }
+
+    override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle) = {
+      i("onCreateView")
+      super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override def onViewCreated(view: View, savedInstanceState: Bundle): Unit = {
+      i("onViewCreated")
+      super.onViewCreated(view, savedInstanceState)
+    }
+
+    override def onStart(): Unit = {
+      i("onStart")
+      super.onStart()
+    }
+
+    override def onResume(): Unit = {
+      i("onResume")
+      super.onResume()
+    }
+
+    override def onPause(): Unit = {
+      i("onPause")
+      super.onPause()
+    }
+
+    override def onStop(): Unit = {
+      i("onStop")
+      super.onStop()
+    }
+
+    override def onDestroyView(): Unit = {
+      i("onDestroyView")
+      super.onDestroyView()
+    }
+
+    override def onDestroy(): Unit = {
+      i("onDestroy")
+      super.onDestroy()
+    }
+
+    override def onDetach(): Unit = {
+      i("onDetach")
+      super.onDetach()
+    }
+  }
+
+  trait Dialog extends AbsDialogFragment with Frmgt
 }
