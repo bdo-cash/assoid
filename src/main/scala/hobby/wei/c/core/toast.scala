c32168a9332c79b0d7c7d3b3b7f54a417790f577
成才 '中© <weichou2010@gmail.com>
2018-10-19 17:19:41 +0800

    upd | toast 支持非UI线程。

--- src/main/scala/hobby/wei/c/core/toast.scala
+++ src/main/scala/hobby/wei/c/core/toast.scala
@@ -17,16 +17,22 @@
 package hobby.wei.c.core
 
 import android.content.Context
+import android.os.Looper
 import android.widget.Toast
+import hobby.chenai.nakam.lang.J2S.{NonNull, Run}
+import hobby.wei.c.core.Ctx.%
 
 /**
   * @author Chenai Nakam(chenai.nakam@gmail.com)
   * @version 1.0, 20/06/2018
   */
-object toast {
+object toast extends %[AbsApp] {
   def apply(s: CharSequence, lang: Boolean = false, gravity: Int = -1, xOffset: Int = 0, yOffset: Int = 0)(implicit context: Context): Unit = {
-    val toast = Toast.makeText(context, s, if (lang) Toast.LENGTH_LONG else Toast.LENGTH_SHORT)
-    if (gravity != -1) toast.setGravity(gravity, xOffset, yOffset)
-    toast.show()
+    val runnable = {
+      val toast = Toast.makeText(context, s, if (lang) Toast.LENGTH_LONG else Toast.LENGTH_SHORT)
+      if (gravity != -1) toast.setGravity(gravity, xOffset, yOffset)
+      toast.show()
+    }.run$
+    if (Looper.myLooper.isNull) getApp.mainHandler.post(runnable) else runnable.run()
   }
 }
