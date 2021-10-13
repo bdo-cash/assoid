ce61b68d21e66eb6efd6f9698d7a4ad7312ef83e
中成才 <chenai.nakam@gmail.com>
2021-10-14 00:31:10 +0800

    + add implicit for 'UiUtils.DpPixelConventions', 'ViewImplicit'.

--- src/main/scala/hobby/wei/c/util/UiUtils.scala
+++ src/main/scala/hobby/wei/c/util/UiUtils.scala
@@ -3,13 +3,31 @@ package hobby.wei.c.util
 import android.app.Activity
 import android.content.Context
 import android.util.TypedValue
-import android.view.{View, Window, WindowManager}
+import android.view.{View, ViewGroup, Window, WindowManager}
+import hobby.chenai.nakam.lang.TypeBring.AsIs
 
 /**
   * @author Wei.Chou
   * @version 1.0, 19/02/2020
   */
 object UiUtils {
+
+  implicit class DipPixelConventions(value: Float) {
+    def dp(implicit context: Context): Float = dp2px(context, value)
+    def dps(implicit context: Context): Int  = dp2pxSize(context, value)
+    def dpo(implicit context: Context): Int  = dp2pxOffset(context, value)
+    def sp(implicit context: Context): Float = sp2px(context, value)
+    def sps(implicit context: Context): Int  = sp2pxSize(context, value)
+    def spo(implicit context: Context): Int  = sp2pxOffset(context, value)
+  }
+
+  implicit class ViewImplicit(view: View) {
+
+    def updateLayoutParams[LP <: ViewGroup.LayoutParams](block: LP => Unit): Unit = {
+      val lp = view.getLayoutParams.as[LP]; block(lp); view.setLayoutParams(lp)
+    }
+  }
+
   def dp2px(context: Context, value: Float): Float =
     TypedValue.applyDimension(
       TypedValue.COMPLEX_UNIT_DIP,
@@ -20,7 +38,7 @@ object UiUtils {
   def dp2pxOffset(context: Context, value: Float): Int = dp2px(context, value).toInt
 
   def dp2pxSize(context: Context, value: Float): Int = {
-    val f = dp2px(context, value)
+    val f        = dp2px(context, value)
     val res: Int = (if (f >= 0) f + 0.5f else f - 0.5f).toInt
     if (res != 0) return res
     if (value == 0f) return 0
@@ -38,7 +56,7 @@ object UiUtils {
   def sp2pxOffset(context: Context, value: Float): Int = sp2px(context, value).toInt
 
   def sp2pxSize(context: Context, value: Float): Int = {
-    val f = sp2px(context, value)
+    val f        = sp2px(context, value)
     val res: Int = (if (f >= 0) f + 0.5f else f - 0.5f).toInt
     if (res != 0) return res
     if (value == 0f) return 0
